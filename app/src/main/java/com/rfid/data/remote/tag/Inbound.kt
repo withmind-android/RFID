1596a33bb52f61c65fa7262a01f15ff29e7c5ce4
kyusikkyu <kyusikkyu@gmail.com>
2021-11-10 14:42:53 +0900

    1차완료

--- app/src/main/java/com/rfid/data/remote/tag/Inbound.kt
+++
@@ -1,5 +0,0 @@
-package com.rfid.data.remote.tag
-
-data class Inbound(
-    val tags: MutableList<String>
-)
--- app/src/main/java/com/rfid/data/remote/tag/Shipment.kt
+++
@@ -1,6 +0,0 @@
-package com.rfid.data.remote.tag
-
-data class Shipment(
-    val tags: MutableList<String>,
-    val images: MutableList<String>
-)
